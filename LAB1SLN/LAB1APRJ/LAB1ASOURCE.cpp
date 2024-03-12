1d7af73cba834e847be7333b81695a0992b930c9
clee7302 <140661033+clee7302@users.noreply.github.com>
2024-03-12 14:34:29 -0500

    Add files via upload

---
+++ LAB1SLN/LAB1APRJ/LAB1ASOURCE.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////
+// Name:
+// Date:
+// Assignment:
+// Description:
+///////////////////////////////////////////////////////
+#include <iostream> //Includes the iostream library
+#include <string> //Includes the string library
+using namespace std; //Assume the use of the standard (std) family
+int main() //the main function
+{ //Start of main
+
+//Type your code here......
+	cout << "This is the source file for ExtraCredit1APrj";
+	cout << endl;
+	cout << endl;
+
+	system("pause"); //Prevents the console from closing on exit.
+	return 0; //Returns whatever is defined by the function return type
+} //End of main
---
+++ LAB1SLN/LAB1BPRJ/LAB1BSOURCE.cpp
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////
+// Name: Christal lee
+// Date: 9/13/2023
+// Assignment: Lab1
+// Description:
+///////////////////////////////////////////////////////
+
+#include <iostream> //Includes the iostream library
+#include <string> //Includes the string library
+
+using namespace std; //Assume the use of the standard (std) family
+
+int main()  //the main function
+{ //Start of main
+
+	//Type your code here......
+	cout << "This is the source file for ExtraCredit1BPrj";
+	cout << endl;
+	cout << endl;
+
+
+	system("pause");  //Prevents the console from closing on exit.  
+	return 0;  //Returns whatever is defined by the function return type
+
+} //End of main
